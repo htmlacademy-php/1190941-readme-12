1dbfd1d75a47483aa4254807a1f0b32e5c95ec4e
1k-bhdn <1k.bhdn@gmail.com>
2021-01-31 14:19:13 +0200

    Описал новую сущность "Репост"

--- schema.sql
+++ schema.sql
@@ -82,3 +82,11 @@ CREATE TABLE subscriptions (
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE reposts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
