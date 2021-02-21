3a49bd6a86f6ddfc363f6bf88a1c8e2bf8f1bb3c
1k-bhdn <1k.bhdn@gmail.com>
2021-02-21 11:55:47 +0200

    Оставил настройки в config.php, дубль 2

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,12 @@
 <?php
 
-$db_host = '127.0.0.1';
-$db_username = 'mysql';
-$db_password = 'mysql';
-$db_database = 'readme_db';
-$db_port = 3306;
-$db_charset = 'utf8mb4';
-
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'username' => 'mysql',
+        'password' => 'mysql',
+        'name' => 'readme_db',
+        'port' => 3306,
+        'charset' => 'utf8mb4',
+    ],
+];
